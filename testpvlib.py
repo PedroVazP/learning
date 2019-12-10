418f1342866da889de17d60ab9e0edbeb1381003
PV <57578524+PedroVazP@users.noreply.github.com>
2019-12-09 23:10:30 -0800

    Create testpvlib.py

---
+++ testpvlib.py
@@ -0,0 +1,33 @@
+import pvlib.store
+import pvlib.hello
+
+#from pvlib import all ¿¿ doesnt work
+
+    #Other subsets
+        #from pvlib import store_st
+        #from pvlib.store_st import all
+        #from pvlib.printhello import printhello
+        #from pvlib.store_st import all
+        #from pvlib.printhello import printhello
+        #from pvlib.store_st import store_st
+
+
+pvlib.store("a")
+pvlib.hello()
+pvlib.store("abcdefghijk")
+pvlib.hello()
+
+
+
+"""
+Reference found
+
+#https://www.digitalocean.com/community/tutorials/how-to-import-modules-in-python-3
+#https://www.digitalocean.com/community/tutorials/how-to-write-modules-in-python-3
+
+#https://docs.python.org/3/reference/import.html
+
+More details. Includes test script.
+#https://stackoverflow.com/questions/34461987/python3-importerror-no-module-named-xxxx
+
+"""
